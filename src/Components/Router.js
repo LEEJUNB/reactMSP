06e1b267f49856cd252e2a42fd02284a437e9a23
junblee <topguide82@gmail.com>
2019-11-26 14:01:07 +0900

    SearchContainer

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 import TV from "Routes/TV";
 import Header from "Components/Header";
 
@@ -13,6 +14,8 @@ export default () => (
                 <Route path="/" exact component={Home}/>
                 <Route path="/tv" component={TV}/>
                 <Route path="/search" component={Search}/>
+                <Route path="/movie/:id" component={Detail} />
+                <Route path="/show/:id" component={Detail} />
                 <Redirect from="*" to="/" /> 
             </Switch>
         </>
